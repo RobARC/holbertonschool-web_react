cd93a8dca0d053ba4862b21368a0dc29645bb111
RobARC <rrondonc@gmail.com>
2022-05-14 11:38:11 -0500

    uploading task 0

---
+++ 0x00-TypeScript/task_0/js/main.ts
@@ -0,0 +1,35 @@
+interface Student {
+    firstName: string;
+    lastName: string;
+    age: number;
+    location: string;
+}
+
+const student1: Student = {
+    firstName: "Robert",
+    lastName: "Rondon",
+    age: 50,
+    location: "Colombia",
+}
+
+const student2: Student = {
+    firstName: "Richard",
+    lastName: "Rondon",
+    age: 52,
+    location: "Venezuela",
+}
+
+const studentList[] = [student1, student2];
+
+const table = document.createElement("table");
+
+studentList.forEach((student) => {
+    let row = table.insertRow();
+    for(let [k, v] of Object.entries(student)){
+        let cell = row.insertCell();
+        let text = document.createTextNode(v);
+        cell.appendChild(text);
+    }
+});
+
+document.body.appendChild(table);

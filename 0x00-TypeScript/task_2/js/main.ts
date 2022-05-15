08d8ef44ec41bc7e02bcd01166297ac89bf81b15
RobARC <rrondonc@gmail.com>
2022-05-15 10:31:45 -0500

    Updating task 5 and uploading task 6

---
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -0,0 +1,69 @@
+interface DirectorInterface {
+    workFromHome(): string
+    getCoffeeBreak(): string
+    workDirectorTasks(): string
+}
+
+interface TeacherInterface {
+    workFromHome(): string
+    getCoffeeBreak(): string
+    workTeacherTasks(): string
+}
+
+class Director implements DirectorInterface {
+    workFromHome(): string {
+        return 'Working from home';
+    }
+    getCoffeeBreak(): string {
+        return 'Getting a coffee break';
+    }
+    workDirectorTasks(): string{
+        return `Getting to director tasks`;
+    }
+};
+
+class Teacher implements TeacherInterface{
+    workFromHome() {
+		return 'Cannot work from home';
+	}
+	getCoffeeBreak() {
+		return 'Cannot have a break';
+	}
+	workTeacherTasks() {
+		return `Getting to work`;
+	}
+}
+
+function createEmployee(salary: number | string){
+    if (typeof(salary) === 'number' && salary < 500)  {
+        return new Teacher();
+    }
+    else{
+        return new Director();
+    }
+}
+
+// Examples
+//console.log(createEmployee(200));
+//console.log(createEmployee(1000));
+//console.log(createEmployee('$500'));
+
+//task 6
+
+function IsDirector(employee: DirectorInterface | TeacherInterface): boolean {
+    return(employee instanceof Director);
+}
+
+function executeWork(employee: DirectorInterface | TeacherInterface): string {
+    if(employee instanceof Director){
+        return employee.workDirectorTasks();
+    }else if (employee instanceof Teacher){
+        return employee.workTeacherTasks();
+    }
+}
+
+//example
+console.log(executeWork(createEmployee(200)));
+//Getting to work
+console.log(executeWork(createEmployee(1000)));
+//Getting to director tasks

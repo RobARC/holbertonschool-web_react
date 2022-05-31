9549aa2ba9db066a31b8e79cbe4c3230d0888da5
RobARC <rrondoc@gmail.com>
2022-05-31 17:04:03 -0500

    uploading task_2

---
+++ 0x01-Webpack/task_2/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+
+module.exports = {
+  mode: 'production',
+  performance: {
+    maxAssetSize: 1000000
+  },
+  entry: './js/dashboard_main.js',
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(gif|png|svg|jpe?g)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+              disable: true
+            }
+          }
+        ]
+      }
+    ]
+  }
+};

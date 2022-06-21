e2bed44a40a034019c1b175c03215d52dd714cd1
RobARC <rrondoc@gmail.com>
2022-06-21 11:32:03 -0500

    updating task 2

--- 0x07-React_Immutable/2-nested.js
+++ 0x07-React_Immutable/2-nested.js
@@ -1,7 +1,8 @@
 import { fromJS } from 'immutable';
 
-export default function accessImmutableObject(object, array) {
+function accessImmutableObject(object, array) {
     const nested = fromJS(object);
     return nested.getIn(array, undefined);
 };
+export default accessImmutableObject;
 

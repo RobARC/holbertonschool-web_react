9bd1fc242983e94f3c7ea61e56ecd07d509745e4
RobARC <rrondoc@gmail.com>
2022-07-09 11:59:57 -0500

    Uploading Task 2

---
+++ 0x04-React_component/task_2/dashboard/src/utils/utils.js
@@ -0,0 +1,17 @@
+export const getFullYear = () => {
+    var current_year = new Date().getFullYear();
+    return current_year;
+}
+
+export const getFooterCopy = (isIndex) => {
+    if (isIndex === true) {
+        return 'Holberton School'
+    } else {
+        return 'Holberton School main dashboard'
+    }
+
+}
+
+export const getLatestNotification = () => {
+    return "<strong>Urgent requirement</strong>- complete by EOD";
+}
